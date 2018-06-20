688f3cc9a2ce8e02f849ef29cf3f2b673732c5d7
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-06-20 17:18:46 -0500

    updated JS

--- js/arrays.js
+++ js/arrays.js
@@ -9,3 +9,24 @@ var texasCities = [sanAntonio, austin, dallas, houston];
 for (var i=0; i < texasCities.length; i++) {
     console.log(texasCities[i]);
 }
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var slice = colors.slice(2, 4);
+
+console.log(colors); // colors is unchanged
+// ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+console.log(slice); // ['yellow', 'green']
+
+slice = colors.slice(3);
+console.log(slice); // ['green', 'blue', 'indigo', 'violet']
+
+//create a function that takes in an array of pies baked and console.logs the 3 most recently baked pies.
+
+function baked(arr) {
+    arr.slice(arr.length - 3);
+    console.log(pieSlice);
+}
+
+baked(pies);
+
---
+++ js/planets-string.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+    console.log(planetsString);
+    var planetsArray = planetsString.split("|");
+    console.log(planetsArray);
+
+
+    var planetsArray;
+    console.log(planetsArray);
+    var planetsString = planetsArray.join("<br>");
+
+    console.log(planetsString);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+
+
+     *
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     *
+     */
+
+    var planetsHTML = "<ul><li>>";
+    planetsHTML += planetsArray.join("</li><li>>");
+    planetsHTML += </li></ul>;
+
+    console.log(planetsHTML);
+
+    document.body.innerHTML = planetsHTML;
+
+})();
