7840bb6cad9f91e4d73757ba151126e0ab2c3de7
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-03 11:54:27 -0500

    jquery traverse exercises

---
+++ js/jquery_faq.js
@@ -0,0 +1,72 @@
+
+"use strict";
+
+
+
+
+// $(function(){
+//     $("#details").click(function () {
+//         $("dd").toggleClass("invisible");
+//
+//     });
+
+
+
+$(document).ready(function () {
+    $("#toggleHideS").click(function () {
+            $("dd").toggle();
+        }
+    )
+
+
+    $("#listButtonHighlight").on("click", function () {
+        $("ul").each(function () {
+            $(this).children().last().toggleClass("highlight");
+        })
+    });
+
+
+    $("li").on("click", function () {
+        $(this).parent().children().first().css({
+            color: "blue"
+        })
+    });
+
+    $("h3").on("click", function () {
+        $(this).next().css("fontWeight", "bold");
+
+    });
+
+
+});
+
+// $("li").last().addClass("highlight");
+
+//
+// $("ul").each(function(index, element) {
+//     $(element).children().last().css("background", "yellow");
+// });
+
+
+
+
+
+
+
+
+
+// $("dd").click(function () {
+//     $(this).next().toggleClass("cursive");
+//
+// })
+
+// $('li').css('background-color', 'yellow');
+
+
+
+// $("h3").css("color", "red");
+
+// $("h3").click(function(){
+//     $(this).next().children().css({
+//         fontWeight:"bold"
+// });
