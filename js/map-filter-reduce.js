d6a6941041ec5f313154c89c68e5251df7190ab9
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-16 20:17:44 -0500

    renamed es6 file

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,72 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+
+let lang = users.filter(user => user.languages.length >=3);
+console.log('lang', lang);
+
+
+
+
+
+
+
+// Use .map to create an array of strings where each element is a user's email address
+
+let email = users.map(user => user.email);
+console.log('em', email);
+
+
+
+
+
+
+
+// Use .reduce to transform the array into an object where the object's keys are ids and the values are objects that represent each user
+
+
+
+
+let newObject = users.reduce((accumalation, currObj) => {
+    console.log(currObj);
+    accumalation[currObj.id] = currObj;
+    return accumalation;
+
+}, {});
+
+console.log(newObject);
+
+
+
