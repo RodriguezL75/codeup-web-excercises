40329c5387b0aba2983d8a251a6c87f661126e98
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-16 20:32:13 -0500

    updated URL and deleted T

--- js/promises.js
+++ js/promises.js
@@ -38,8 +38,8 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 //     });
 //     getLastEvent(username);
 
-const userName = "RodriguezL75";
-const token = '6b19cda6d236765d1ac56c137ebdffbb0bad5740';
+const userName = "";
+const token = '';
 const url = `https://api.github.com/users/${userName}/events/public`;
 
 const getLastEvent = ((userName)=> {
@@ -61,7 +61,7 @@ const getLastEvent = ((userName)=> {
 
 //     const username = `ruusabolton`;//define the username you are targetting
 //     const url = `https://api.github.com/users/${username}/events/public`;//URL of API
-//     const token = "1bde1e4157eadde19c09f3b4f5881e2905c05f9d";//your GitHub token
+//     const token = "1";//your GitHub token
 //
 // //declare function that 1. fetches the data, 2. puts data in json format, 3. console.log the data
 // //so you can read it once you have called the function
